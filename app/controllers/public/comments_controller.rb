5830472a94dddff08d8d4d9316278010136146cb
suguto <misu09190120@gmail.com>
2022-07-13 01:28:45 +0000

    [add] comment-system to exercise_show

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,2 +1,29 @@
 class Public::CommentsController < ApplicationController
+
+  def create
+    @exercise = Exercise.find(params[:exercise_id])
+    @comment = current_customer.comments.new(comment_params)
+    @comment.exercise_id = @exercise.id
+    if @comment.save
+      render 'exercise_show_comments'
+    else
+      flash[:notice] = "入力してください"
+      @comment_new = Comment.new
+      render 'public/exercises/show'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    @exercise = Exercise.find(params[:exercise_id])
+    render 'exercise_show_comments'
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment_to)
+  end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
-  
+
   belongs_to :customer
-  belongs_to :exercises
+  belongs_to :exercise
+
+  validates :comment_to, presence: true
 end
